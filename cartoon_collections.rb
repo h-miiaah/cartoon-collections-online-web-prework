5e4c6be31ef041edccf63c293dd30b4d7bfa8965
Haim D <haim11293@aol.com>
2019-06-29 00:50:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-
+  planeteer_calls.map! {|planeteer_call| planeteer_call.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
